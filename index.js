ea8326bd1a705c7c8ebf104197736e5db0ec67ba
divyab979 <divyab979@gmail.com>
2026-04-30 16:53:33 +0530

    port chages

--- index.js
+++ index.js
@@ -472,6 +472,8 @@ Return JSON:
 });
 
 // ================= START =================
-app.listen(3001, () => {
-  console.log("Server running on port 3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
